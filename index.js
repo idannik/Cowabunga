ce480b3397c7833af8fc8836279a64b53a14359a
Idan Nikritin <idan.nik@gmail.com>
2017-08-05 17:55:51 +0300

    add webhook

--- index.js
+++ index.js
@@ -35,3 +35,14 @@ app.get('/db', function (request, response) {
     });
   });
 });
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'MichelAngelo') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
